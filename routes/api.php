76b97cd9864280f001727ed97ebec60eb82b7223
Giafn <giafauzan11@gmail.com>
2024-07-04 17:07:28 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\HooksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::post('/hooks/deploy/{repoName}', [HooksController::class, 'handleGithubWebhookForDeploy']);
