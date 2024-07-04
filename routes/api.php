dd08b8c96395925f84560afa2ffef021112bfbb6
Giafn <giafauzan11@gmail.com>
2024-07-04 12:33:12 +0700

    test

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\HooksController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('/hooks/deploy', [HooksController::class, 'handleGithubWebhookForDeploy']);
