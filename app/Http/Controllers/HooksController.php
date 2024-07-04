a3881f51853db17581fa7d7e246b48f42afee2c4
Giafn <giafauzan11@gmail.com>
2024-07-04 14:35:58 +0700

    update

--- app/Http/Controllers/HooksController.php
+++ app/Http/Controllers/HooksController.php
@@ -11,35 +11,42 @@ class HooksController extends Controller
     public function handleGithubWebhookForDeploy(Request $request, $repoName)
     {
         echo "webhook received";
-        Log::info('webhook received');
-        $requestHash = $request->header('x-hub-signature-256') ?? '';
-        $payload = $request->getContent();
+        try {
+            Log::info('webhook received');
+            $requestHash = $request->header('x-hub-signature-256') ?? '';
+            $payload = $request->getContent();
 
-        $localHash = 'sha256='.hash_hmac('sha256', $payload, env('APP_DEPLOY_SECRET'));
+            $localHash = 'sha256='.hash_hmac('sha256', $payload, env('APP_DEPLOY_SECRET'));
 
-        if(!hash_equals($localHash, $requestHash)){
-            return response()->json(['error' => 'invalid-signature'], 200);
+            if(!hash_equals($localHash, $requestHash)){
+                return response()->json(['error' => 'invalid-signature'], 200);
+            }
+
+            echo "state 0";
+            
+            $json = json_decode($payload);
+            if(!isset($json->ref)){
+                return response()->json(['error' => 'invalid-push_payload'], 200);
+            }
+
+            if( $json->ref != "refs/heads/main"){
+                return response()->json(['success' => 'not-main'], 200);
+            }
+            $folderApp = $this->getBasePath(env('APP_FOLDER'));
+            $repoLocation = $folderApp . '/' . $repoName;
+            $scriptPath = $repoLocation . '/deploy.sh';
+            echo "state 1";
+            $output = shell_exec("bash $scriptPath 2>&1");
+            
+            log::info("lokasi repo: $repoLocation");
+            log::info("lokasi script: $scriptPath");
+            Log::info("result webhook: $output");
+
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            echo $e->getMessage();
+            return response()->json(['error' => $e->getMessage()], 200);
         }
-
-        echo "state 0";
-        
-        $json = json_decode($payload);
-        if(!isset($json->ref)){
-            return response()->json(['error' => 'invalid-push_payload'], 200);
-        }
-
-        if( $json->ref != "refs/heads/main"){
-            return response()->json(['success' => 'not-main'], 200);
-        }
-        $folderApp = $this->getBasePath(env('APP_FOLDER'));
-        $repoLocation = $folderApp . '/' . $repoName;
-        $scriptPath = $repoLocation . '/deploy.sh';
-        echo "state 1";
-        $output = shell_exec("bash $scriptPath 2>&1");
-        
-        log::info("lokasi repo: $repoLocation");
-        log::info("lokasi script: $scriptPath");
-        Log::info("result webhook: $output");
 
         return response()->json(['success' => 'success'], 200);
     }
